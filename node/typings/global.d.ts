2d6b23e6dae55120a580bac338be7276d8559519
Sávio Muniz <savio.muniz@vtex.com.br>
2020-06-17 15:58:16 -0300

    restructuring to include configuration builder and channels setup

--- node/typings/global.d.ts
+++ node/typings/global.d.ts
@@ -13,3 +13,38 @@ interface SetupState {
   docks?: LogisticsConfigurations[]
   warehouses?: LogisticsConfigurations[]
 }
+
+interface ChannelRequest {
+  requester: string
+  requested: string
+  salesChannels: SalesChannelMap[]
+  settings: AppSettings
+}
+
+interface AppSettings extends ChannelSettings {
+  appId: string
+  name: string
+  imageUrl: string
+  salesChannels: SalesChannelSettings[]
+}
+
+interface SalesChannelMap {
+  affiliateId: string
+  mkp: number
+  seller: number
+}
+
+interface SalesChannelState extends SalesChannelMap {
+  currency: string
+}
+
+interface SalesChannelSettings {
+  id: number
+  name: string
+  description: string
+  commissioning: {
+    product: number
+    freight: number
+  }
+  payment: string
+}
